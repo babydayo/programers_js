2e2a83c3b831b50ae8f9b302cae6be29a0ac481c
dayo <tjdguschoi@gmail.com>
2025-09-27 17:37:55 +0900

    입문문제

---
+++ 입문문제/짝수의 합.js
@@ -0,0 +1,13 @@
+// 정수 n이 주어질 때, n이하의 짝수를 모두 더한 값을 return 하도록 solution 함수를 작성해주세요.
+
+const solution = n => {
+    let result = 0;
+
+    for (i = 0; i <= n; i++ ) {
+        if (i % 2 === 0) {
+            result = result + i
+        }
+    }
+
+    return result;
+}
