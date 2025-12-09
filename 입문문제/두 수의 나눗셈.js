b30c72fd5b9d01322671c164336af8d7a14d12a1
dayo <tjdguschoi@gmail.com>
2025-12-09 22:43:35 +0900

    251209 입문문제

--- 입문문제/두 수의 나눗셈.js
+++ 입문문제/두 수의 나눗셈.js
@@ -1,8 +1,8 @@
 // 정수 num1과 num2가 매개변수로 주어질 때,
 // num1을 num2로 나눈 값에 1,000을 곱한 후 정수 부분을 return 하도록 solution 함수를 완성해주세요.
+// 25.12.09
 
 const solution = (num1, num2) => {
-    const result = num1 / num2 * 1000
-
-    return Math.floor(result);
-};
+    // return Math.floor(num1 / num2 * 1000);
+    return Math.trunc(num1 / num2 * 1000);
+}
--- 입문문제/숫자 비교하기.js
+++ 입문문제/숫자 비교하기.js
@@ -1,4 +1,5 @@
 // 정수 num1과 num2가 매개변수로 주어집니다. 두 수가 같으면 1 다르면 -1을 retrun하도록 solution 함수를 완성해주세요.
+// 25.12.09
 
 const solution = (num1, num2) => {
     return num1 === num2 ? 1 : -1;
