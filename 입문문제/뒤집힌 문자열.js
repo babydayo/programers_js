b677d4fad2588a703c0e6bd1251fa4b9905de2ba
dayo <tjdguschoi@gmail.com>
2025-12-17 22:51:51 +0900

    251217 입문문제

--- 입문문제/뒤집힌 문자열.js
+++ 입문문제/뒤집힌 문자열.js
@@ -1,8 +1,11 @@
 // 문자열 my_string이 매개변수로 주어집니다.
 // my_string을 거꾸로 뒤집은 문자열을 return하도록 solution 함수를 완성해주세요.
+// 25.12.17
+
+// const solution = (my_string) => {
+//     return [...my_string].reverse().join('');
+// }
 
 const solution = (my_string) => {
-    const result = [...my_string].reverse().join('');
-    
-    return result
+    return my_string.split('').reverse().join('');
 }
