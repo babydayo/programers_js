e1f140fa99ef8bc3d5ace5284f68824a6381d620
dayo <tjdguschoi@gmail.com>
2025-12-22 22:52:43 +0900

    251222 입문문제

---
+++ 입문문제/문자 출력 반복하기.js
@@ -0,0 +1,21 @@
+// 문자열 my_string과 정수 n이 매개변수로 주어질 때,
+// my_string에 들어있는 각 문자를 n만큼 반복한 문자열을 return 하도록 solution 함수를 완성해보세요.
+// 25.12.22
+
+// const solution = (my_string, n) => {
+//     const resultArr = my_string.split('').map((str) => {
+//         let resultText = '';
+
+//         for(let i=0; i<n; i++){
+//             resultText += str;
+//         }
+        
+//         return resultText;
+//     })
+    
+//     return resultArr.join('');
+// }
+
+const solution = (my_string, n) => {
+    return [...my_string].map((str) => str.repeat(n)).join('');
+}
