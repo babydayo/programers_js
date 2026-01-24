f6e6cd760593e1990d7b0450f933c5fef288f80d
dayo <tjdguschoi@gmail.com>
2026-01-24 16:51:00 +0900

    260124 입문문제

---
+++ 입문문제/중복된 숫자 개수.js
@@ -0,0 +1,7 @@
+// 정수가 담긴 배열 array와 정수 n이 매개변수로 주어질 때,
+// array에 n이 몇 개 있는 지를 return 하도록 solution 함수를 완성해보세요.
+// 26.01.24
+
+const solution = (array, n) => {
+    return array.filter((num) => num === n).length;
+}
